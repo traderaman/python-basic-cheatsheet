098542821d737248c35b3a00ba4c629b7fb20263
Aman Singh <71070999+traderaman@users.noreply.github.com>
2021-08-13 22:51:19 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -117,7 +117,7 @@ elif name == 'Sammy':
     print('Hi Sammy')
 else:
     print('What is you name?')
-    
+
 #FOR LOOP
 mylist = [1,2,3]
 for item in mylist:
@@ -129,5 +129,103 @@ list = [1,2,3,4,5,6,7,8,9,10]
 for letter in list:
     if letter % 2 == 0:
         print(letter)
+    else:
+        print(f'Odd number: {letter} ')
+
+mystring = 'Hello'
+for letters in mystring:
+    print(letters)
+
+newlist = [(1,2),(3,4),(5,6)]
+for a,b in newlist:
+    print(a)
+
+dic = {'k':'v','k2':'v2','k3':'v3'}
+for item in dic:
+    print(item)
+for values in dic.values():
+    print(values)
+
+#WHILE LOOP
+x = 0
+while x < 5:
+    print(f'The value of x is {x}')
+    x = x + 1  #if not provided there will be unlimited loop, as 'x' has no value.
+
+x = 0
+while x < 5:
+    print(f'The value of x is {x}')
+    x = x + 1
+else:
+    print('x is not less than 5')
+
+#PASS
+x = [1,2,3]
+for item in x:
+    pass
+print('end of script')
+
+#CONTINUE
+string = 'Sammy'
+for letter in string:
+    if letter == 'a':
+        continue
+    print(letter)
+
+#BREAK
+x = 0
+while x < 5:
+    if x == 2:
+        break
+    print(x)
+    x = x+1
+
+#RANGE FUNCTION
+mylist = [1,2,3]
+for num in range(10):
+    print(num)
+
+mylist = [1,2,3]
+for num in range(3,10):
+    print(num)
+
+mylist = [1,2,3]
+for num in range(3,11):
+    print(num)
+
+#ENUMERATE FUNCTION
+count = 0
+for letter in 'abcde':
+    print('At index {} the letter is {}'.format(count,letter))
+    count = count + 1
+
+word = 'abcde'
+for item in enumerate(word):
+    print(item)
+
+#ZIP FUNCTION
+list1 = [1,2,3]
+list2 = ['a','b','c']
+for item in zip(list1,list2):
+    print(item)
+
+#IN
+print('b' in ('x','y','z'))
+print('mykey' in {'mykey':345})
+
+d = {'mykey':345}
+print(345 in d.values())
+
+#MIN MAX
+mylist = [10,20,30,40,50]
+print(min(mylist))
+print(max(mylist))
+
+#IMPORT FUNCTIONS FROM LIBRARY
+from random import shuffle
+
+#INPUT
+result = int(input('Your age is: '))  #int/float is used to take value as integer not as string
+print(result)
 
 #####END######
